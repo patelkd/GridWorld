1bb9bcfa22824a18e9bb28cbb4b0467a01488f1b
Karishma Patel <karishma1126@yahoo.com>
2014-01-14 09:08:40 -0700

    z bug

--- ZBug/ZBug.java
+++ ZBug/ZBug.java
@@ -4,7 +4,7 @@ import info.gridworld.actor.Bug;
  * A <code>BoxBug</code> traces out a square "box" of a given size. <br />
  * The implementation of this class is testable on the AP CS A and AB exams.
  */
-public class CircleBug extends Bug
+public class ZBug extends Bug
 {
     private int steps;
     private int sideLength;
@@ -13,10 +13,11 @@ public class CircleBug extends Bug
      * Constructs a box bug that traces a square of a given side length
      * @param length the side length
      */
-    public CircleBug(int length)
+    public ZBug(int length)
     {
+        this.setDirection(90); 
         steps = 0;
-        sideLength = length;
+        sideLength = 4;
     }
 
     /**
@@ -31,13 +32,12 @@ public class CircleBug extends Bug
         }
         else
         {
+
+            sideLength = 4;
+            turn();
+            turn();
             turn();
             steps = 0;
         }
     }
 }
-
-
-//The behavior is different from BoxBug because one turn results in 45 degrees.
-//If the bug is able to move at this angle, it will. This is different than 
-//BoxBug as BoxBug only turns at 90 degrees.
--- spiralBug/spiralBug.java
+++ spiralBug/spiralBug.java
@@ -1,10 +1,11 @@
+
 import info.gridworld.actor.Bug;
 
 /**
  * A <code>BoxBug</code> traces out a square "box" of a given size. <br />
  * The implementation of this class is testable on the AP CS A and AB exams.
  */
-public class CircleBug extends Bug
+public class spiralBug extends Bug
 {
     private int steps;
     private int sideLength;
@@ -13,7 +14,7 @@ public class CircleBug extends Bug
      * Constructs a box bug that traces a square of a given side length
      * @param length the side length
      */
-    public CircleBug(int length)
+    public spiralBug(int length)
     {
         steps = 0;
         sideLength = length;
@@ -31,13 +32,10 @@ public class CircleBug extends Bug
         }
         else
         {
+            sideLength = sideLength +1;
+            turn();
             turn();
             steps = 0;
         }
     }
 }
-
-
-//The behavior is different from BoxBug because one turn results in 45 degrees.
-//If the bug is able to move at this angle, it will. This is different than 
-//BoxBug as BoxBug only turns at 90 degrees.
