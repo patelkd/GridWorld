032ebc45a1a775e83f00f3ea5119114fcd081664
Karishma Patel <karishma1126@yahoo.com>
2014-02-06 10:13:30 -0700

    added lighen and darken

--- Part4Exercises/BlusterCritter/BlusterCritter.java
+++ Part4Exercises/BlusterCritter/BlusterCritter.java
@@ -11,25 +11,19 @@ public class BlusterCritter extends Critter
 {
 	private int c;
     private int counter;
+    private static final double DARKENING_FACTOR = 0.05;
+
 
 	public BlusterCritter() {
 		int c = 12;
 	}
 
+    public ArrayList getCritters() {
 
-    public void processActors(ArrayList<Actor> actors) {
-     for (Actor a : actors)
-     {
-         if ((a instanceof Critter)) {
-             counter ++;
-         }
-      } 
-    }
-    
+        ArrayList<Actor> actors = new ArrayList<Actor>(); 
 
-    public void getCritters() {
         Location loc = getLocation();
-        for (int r=loc.getRow() -2; r<= loc.getRow() +2; r++) {
+        for (int r=loc.getRow()-2; r<=loc.getRow() +2; r++) {
             for (int c=loc.getCol()-2; c <= loc.getCol() +2 ; c++) {
                 Location temporary = new Location(r,c);
                 if (getGrid().isValid(temporary)) {
@@ -38,12 +32,48 @@ public class BlusterCritter extends Critter
                         actors.add(a);
                     }
                 }
-                return actors;
+                
             }
         }
+        return actors;
+    }
+
+
+    public void processActors(ArrayList<Actor> actors) {
+     for (Actor a : actors)
+     {
+         if ((a instanceof Critter)) {
+             counter ++;
+         }
+         if (counter < c) {
+            lighten();
+         } else {
+            darken();
+         }
+
+      } 
 
     }
 
+    public void darken() {
+
+        Color c = getColor();
+        int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
+        int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
+        int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
+
+        setColor(new Color(red, green, blue));
+    }
+
+    public void lighten() {
+        Color c = getColor();
+        int red = (int) (c.getRed() * (DARKENING_FACTOR-1));
+        int green = (int) (c.getGreen() * (DARKENING_FACTOR-1));
+        int blue = (int) (c.getBlue() * (DARKENING_FACTOR-1));
+
+        setColor(new Color(red, green, blue));
+    }
+    
 
 }
   
